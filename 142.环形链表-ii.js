5b1ce486d5234d9dd2084092be8261a119878b8a
Jason_Shu <shuyue524@gmail.com>
2020-02-03 23:30:57 +0800

    done 142

--- 142.环形链表-ii.js
+++ 142.环形链表-ii.js
@@ -17,8 +17,39 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+//  小技巧：slow和fast指针都从head开始，第一次碰面后，将fast重置到head
+// 然后slow和fast指针分别步长1前进，再次相遇时就是「环头部」
 var detectCycle = function(head) {
-    
+    if(head === null || head.next === null) return null;
+
+    let slow = head;
+    let fast = head;
+    let pos = -1;
+
+    slow = slow.next;
+
+    fast = fast.next.next;
+
+    while(fast !== null && fast.next !== null) {
+        if(slow === fast) {
+            fast = head;
+            break;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    if(fast === head) {
+        while(slow !== fast) {
+            pos++;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    } 
+
+    return null;
 };
 // @lc code=end
 
